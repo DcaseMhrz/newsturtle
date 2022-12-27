ea2dcb1247002824ebc63edc9da1a80aee667b5e
Dikesh Maharjan <dcasemhrz@gmail.com>
2022-12-27 12:38:47 +1100

    components setup

---
+++ src/components/News.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>This is news based components
+        <NewsItem/>
+
+      </div>
+      
+    )
+  }
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>This is news item component</div>
+    )
+  }
+}
+
+export default NewsItem
